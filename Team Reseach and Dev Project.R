18068f3269b082480184fd31f39bacf98909b362
S121472 <sa24abf@herts.ac.uk>
2024-11-22 00:42:53 +0500

    Update Team Reseach and Dev Project.R

--- Team Reseach and Dev Project.R
+++ Team Reseach and Dev Project.R
@@ -1,9 +1,5 @@
-
-
-#Guys if you are seeing this you need to first install all the packages. Link repository me hai
-
-
-
+head(rio_csv)
+options(scipen = 999)
 
 #SAVE EACH CLASS OF EXPERIENCE LEVEL INDEPENDENTLY
 entry_level <- rio_csv$salary_in_usd [rio_csv$experience_level == "EN"]
@@ -17,8 +13,14 @@ hist(mid_level,breaks=20, col = "red")
 hist(senior_level,breaks=20, col = "red")
 hist(experienced_level,breaks=20, col = "red")
 
+
 #...............................................
 #checking normality
 boxplot(rio_csv$salary_in_usd ~ rio_csv$experience_level, xlab= "experience levels", ylab= "salary", main="LOL")
 options(scipen = 999)
-hist(rio_csv$salary_in_usd, breaks =20, col = "blue") 
+hist(rio_csv$salary_in_usd, breaks =20, col = "blue")
+#data is in bell shape so use pairwise student T test
+
+pairwise_t_result <- pairwise.t.test(rio_csv$salary_in_usd, rio_csv$experience_level, p.adjust.method = "bonferroni")
+
+print(pairwise_t_result)
